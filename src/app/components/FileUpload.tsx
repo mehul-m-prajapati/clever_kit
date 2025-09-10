b073642e264ea76d709dda7eedd4c8d4c52657bb
Mehul <mehulprajapati2802@gmail.com>
2025-09-10 15:21:04 +0530

    added header, file upload and notification components

--- src/app/components/FileUpload.tsx
+++ src/app/components/FileUpload.tsx
@@ -1,8 +1,89 @@
 "use client";
 
-function FileUpload() {
+import { useState } from "react";
+import {
+    ImageKitAbortError,
+    ImageKitInvalidRequestError,
+    ImageKitServerError,
+    ImageKitUploadNetworkError,
+    upload,
+} from "@imagekit/next"
+
+
+interface FileUploadProps {
+  onSuccess: (res: any) => void;
+  onProgress_?: (progress: number) => void;
+  fileType?: "image" | "video";
+}
+
+function FileUpload({ onSuccess, onProgress_, fileType }: FileUploadProps) {
+
+  const [uploading, setUploading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const validateFile = (file: File) => {
+
+    if (fileType === "video") {
+        if (!file.type.startsWith("video/")) {
+            setError("Please upload a valid video file");
+        }
+    }
+
+    if (file.size > 100 * 1024 * 1024) {
+        setError("File size must be less than 100 MB");
+    }
+
+    return true;
+  }
+
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+
+    const file = e.target.files?.[0];
+
+    if (!file || !validateFile(file))
+        return;
+
+    setUploading(true);
+    setError(null);
+
+    try {
+        const resp = await fetch("/api/auth/imagekit-auth");
+        const {authenticationParameters: auth_params} = await resp.json();
+
+        const result = await upload({
+            file,
+            fileName: file.name,
+            publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY!,
+            signature: auth_params.signature,
+            expire: auth_params.expire,
+            token: auth_params.token,
+            onProgress: (event) => {
+                const percent = (event.loaded / event.total) * 100;
+                onProgress_?.(Math.round(percent));
+            }
+        });
+
+        onSuccess(result);
+    }
+    catch (error) {
+        console.error("Upload failed", error)
+    }
+    finally {
+        setUploading(false);
+    }
+  }
+
   return (
-    <div>FileUpload</div>
+    <div>
+        <input
+            type="file"
+            accept={fileType === "video" ? "video/*" : "image/*"}
+            onChange={handleFileChange}
+        />
+        {uploading && <span>Loading....</span>}
+
+        {error && <p className="text-red-800 text-xl font-bold">{error}</p>}
+    </div>
   )
 }
 
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,31 +3,31 @@ import { NextResponse } from "next/server";
 
 export default withAuth(
 
-  function middleware() {
-    return NextResponse.next();
-  },
-
-  {
-    callbacks: {
-
-      authorized({ req, token }) {
-        const { pathname } = req.nextUrl;
-
-        if (
-          pathname.startsWith("/api/auth") ||
-          pathname === "/login" ||
-          pathname === "/register"
-        )
-          return true;
-
-        if (pathname === "/" || pathname.startsWith("/api/videos")) {
-          return true;
-        }
-
-        return !!token;
-      },
+    function middleware() {
+        return NextResponse.next();
     },
-  }
+
+    {
+        callbacks: {
+            authorized({ req, token }) {
+                const { pathname } = req.nextUrl;
+
+                if (
+                    pathname.startsWith("/api/auth") ||
+                    pathname === "/login" ||
+                    pathname === "/register"
+                ) {
+                    return true;
+                }
+
+                if (pathname === "/" || pathname.startsWith("/api/videos")) {
+                    return true;
+                }
+
+                return !!token;
+            },
+        },
+    }
 );
 
 export const config = {
